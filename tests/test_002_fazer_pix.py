2a4373712ec329ccff12f47ac827717b10ec37e3
Thiago Rodrigues <thiago.r.brito048@gmail.com>
2026-05-11 11:13:34 -0300

    Teste Estudo

--- tests/test_002_fazer_pix.py
+++ tests/test_002_fazer_pix.py
@@ -7,7 +7,7 @@ def test_002_fazer_pix(common_page,login_page, home_page,pix_page) -> None:
     pix_page.voltar_home()
     common_page.assert_text("4.990,00")
     home_page.acessar_menu("Ver Extrato")
-    common_page.assert_text("11/05/2026 - Pix para 999.999").to_be_visible(timeout=10000)
+    common_page.assert_text("11/05/2026 - Pix para 999.999")
     
 
 
